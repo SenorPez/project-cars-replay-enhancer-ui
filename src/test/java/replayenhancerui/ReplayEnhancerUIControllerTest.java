a5521ef17775a6d8c79b1a4134f30fa50084a090
SenorPez <senorpez@senorpez.com>
2016-08-08 13:12:58 -0500

    Reorganization of dependencies.

---
+++ src/test/java/replayenhancerui/ReplayEnhancerUIControllerTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package replayenhancerui;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author SenorPez
+ */
+public class ReplayEnhancerUIControllerTest {
+    
+    public ReplayEnhancerUIControllerTest() {
+    }
+    
+    @Test
+    public void TestTest() {
+        assertTrue(true);
+    }
+
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+    
+}
