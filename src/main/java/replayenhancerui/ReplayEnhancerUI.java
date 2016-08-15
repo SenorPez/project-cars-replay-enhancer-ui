1e242c15893e228ea91b098e25d771fafc5121e6
SenorPez <senorpez@senorpez.com>
2016-08-15 12:19:10 -0500

    Updating interface.

--- src/main/java/replayenhancerui/ReplayEnhancerUI.java
+++ src/main/java/replayenhancerui/ReplayEnhancerUI.java
@@ -7,8 +7,10 @@ package replayenhancerui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -20,11 +22,23 @@ public class ReplayEnhancerUI extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ReplayEnhancerUI.fxml"));
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
+        stage.setX(primaryScreenBounds.getMinX());
+        stage.setY(primaryScreenBounds.getMinY());
         stage.show();
+        stage.setMinWidth(stage.getWidth());
+        stage.setMaxWidth(stage.getWidth());
+        stage.setMaxHeight(stage.getHeight());
+        
+        if (stage.getHeight() > primaryScreenBounds.getHeight()) {
+            stage.setHeight(primaryScreenBounds.getHeight()*.90);
+        }
+        
+        stage.setTitle("Project CARS Replay Enhancer Configuration Editor");
     }
 
     /**
